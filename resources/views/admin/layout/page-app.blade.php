35a6f81df1c6bfb482751db105e0210812910aa8
moldokipgen23 <darpu05@gmail.com>
2026-05-31 11:42:29 +0530

    Fix confirm modal: event delegation, remove Bootstrap 5 conflict, fix duplicate body tag

--- resources/views/admin/layout/page-app.blade.php
+++ resources/views/admin/layout/page-app.blade.php
@@ -64,12 +64,10 @@
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/js/popper.min.js') }}"></script>
     <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/js/js.js')}}"></script>
     <!-- Datatable -->
     <script src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('assets/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('assets/js/js.js')}}"></script>
     <!-- Toastr -->
     <script src="{{ asset('assets/js/toastr.min.js')}}"></script>
 
@@ -281,7 +279,7 @@
                 onConfirm: function() { window.location.href = url; }
             });
         }
-        $('#confirmModalBtn').on('click', function() {
+        $(document).on('click', '#confirmModalBtn', function() {
             $('#confirmModal').modal('hide');
             if (confirmModalCallback) {
                 confirmModalCallback();
@@ -312,7 +310,4 @@
     </div>
 
     @yield('pagescript')
-</body>
-</body>
-
-</html>
+</body></html>
